52f9f544b66b3ff9df75aa695e463920013621ea
vuonganh283 <anhananh283@gmail.com>
2025-08-11 09:20:32 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,213 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cng-vo",
+      "name": "Cổng vào",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3343787490306269,
+          "pitch": 0.09475144586243367,
+          "rotation": 0,
+          "target": "1-trung-tm-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-trung-tm-",
+      "name": "Trung tâm ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6111347512226768,
+          "pitch": 0.23362652707256792,
+          "rotation": 0,
+          "target": "2-phng-thi-cng-phi"
+        },
+        {
+          "yaw": -0.5993106280655063,
+          "pitch": 0.2459109261059762,
+          "rotation": 0,
+          "target": "3-phng-thi-cng-tri"
+        },
+        {
+          "yaw": -1.5724233346643874,
+          "pitch": 0.1977235017998158,
+          "rotation": 0,
+          "target": "4-phng-khch"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-phng-thi-cng-phi",
+      "name": "Phòng thi công phải",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.012684194239675,
+          "pitch": 0.258433626249019,
+          "rotation": 0,
+          "target": "1-trung-tm-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-phng-thi-cng-tri",
+      "name": "Phòng thi công trái",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0402419590339917,
+          "pitch": 0.2547550000702863,
+          "rotation": 0,
+          "target": "1-trung-tm-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-phng-khch",
+      "name": "Phòng khách",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.921908742984761,
+          "pitch": 0.12692214568278537,
+          "rotation": 0,
+          "target": "1-trung-tm-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
